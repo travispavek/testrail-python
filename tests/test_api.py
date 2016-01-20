b1ad56c7dfe04c1b3c4b665bbec6a475c0298fec
Travis Pavek <travis@travispavek.com>
2016-01-19 21:17:28 -0600

    Add Project API GET test coverage.

--- tests/test_api.py
+++ tests/test_api.py
@@ -202,5 +202,111 @@ class TestUser(unittest.TestCase):
         err_msg = "User email 'invalid@example.com' was not found"
         self.assertEqual(err_msg, str(e.exception))
 
+
+class TestProject(unittest.TestCase):
+    def setUp(self):
+        self.client = API()
+        self.mock_project_data = [
+            {
+                "announcement": "..",
+                "completed_on": None,
+                "id": 1,
+                "is_completed": False,
+                "name": "Project1",
+                "show_announcement": True,
+                "url": "http://<server>/testrail/index.php?/projects/overview/1"
+            },
+            {
+                "announcement": "..",
+                "completed_on": False,
+                "id": 2,
+                "is_completed": True,
+                "name": "Project2",
+                "show_announcement": True,
+                "url": "http://<server>/testrail/index.php?/projects/overview/2"
+            }
+        ]
+
+        self.projects = copy.deepcopy(self.mock_project_data)
+
+    def tearDown(self):
+        util.reset_shared_state(self.client)
+
+    @mock.patch('testrail.api.requests.get')
+    def test_get_projects(self, mock_get):
+        mock_response = mock.Mock()
+        expected_response = self.projects
+        url = 'https://<server>/index.php?/api/v2/get_projects'
+        mock_response.json.return_value = self.mock_project_data
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+        actual_response = self.client.projects()
+        timeout = self.client._timeout
+        # set timeout to 1 second from now
+        delta = timedelta(seconds=timeout-1)
+        self.client._users['ts'] = datetime.now() - delta
+        actual_response = self.client.projects()  # verify cache hit
+        mock_get.assert_called_once_with(
+            url,
+            headers={'Content-Type': 'application/json'},
+            params=None,
+            auth=('user@yourdomain.com', 'your_api_key')
+        )
+        self.assertEqual(1, mock_response.json.call_count)
+        self.assertEqual(expected_response, actual_response)
+
+    @mock.patch('testrail.api.requests.get')
+    def test_get_projects_cache_timeout(self, mock_get):
+        mock_response = mock.Mock()
+        expected_response = self.projects
+        url = 'https://<server>/index.php?/api/v2/get_projects'
+        mock_response.json.return_value = self.mock_project_data
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+        actual_response = self.client.projects()
+        timeout = self.client._timeout
+        self.client._projects['ts'] = datetime.now() - timedelta(
+            seconds=timeout)
+        actual_response = self.client.projects()  # verify cache hit
+        mock_get.assert_called_twice_with(
+            url,
+            headers={'Content-Type': 'application/json'},
+            params=None,
+            auth=('user@yourdomain.com', 'your_api_key')
+        )
+        self.assertEqual(2, mock_response.json.call_count)
+        self.assertEqual(expected_response, actual_response)
+
+    @mock.patch('testrail.api.requests.get')
+    def test_get_project_id(self, mock_get):
+        mock_response = mock.Mock()
+        expected_response = filter(
+            lambda x: x if x['id'] == 1 else None, self.projects)[0]
+        url = 'https://<server>/index.php?/api/v2/get_projects'
+        mock_response.json.return_value = self.mock_project_data
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+        actual_response = self.client.project_with_id(1)
+        actual_response = self.client.project_with_id(1)  # verify cache hit
+        mock_get.assert_called_once_with(
+            url,
+            headers={'Content-Type': 'application/json'},
+            params=None,
+            auth=('user@yourdomain.com', 'your_api_key')
+        )
+        self.assertEqual(1, mock_response.json.call_count)
+        self.assertEqual(expected_response, actual_response)
+
+    @mock.patch('testrail.api.requests.get')
+    def test_get_project_invalid_id(self, mock_get):
+        mock_response = mock.Mock()
+        mock_response.json.return_value = self.mock_project_data
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+        with self.assertRaises(TestRailError) as e:
+            self.client.project_with_id(300)
+        err_msg = "Project ID '300' was not found"
+        self.assertEqual(err_msg, str(e.exception))
+
 if __name__ == "__main__":
     unittest.main()
