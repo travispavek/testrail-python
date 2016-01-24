8b828826a6244dc652bdbb8c5a5e717a3dcc17bc
Travis Pavek <travis@travispavek.com>
2016-01-23 20:19:13 -0600

    Rename type check tests.

--- tests/test_user.py
+++ tests/test_user.py
@@ -16,23 +16,23 @@ class TestUser(unittest.TestCase):
     def test_get_email(self):
         self.assertEqual(self.user.email, 'han@example.com')
 
-    def test_email_type(self):
+    def test_get_email_type(self):
         self.assertEqual(str, type(self.user.email))
 
     def test_get_id(self):
         self.assertEqual(self.user.id, 1)
 
-    def test_id_type(self):
+    def test_get_id_type(self):
         self.assertEqual(int, type(self.user.id))
 
     def test_get_is_active(self):
         self.assertEqual(self.user.is_active, True)
 
-    def test_is_active_type(self):
+    def test_get_is_active_type(self):
         self.assertEqual(bool, type(self.user.is_active))
 
     def test_get_name(self):
         self.assertEqual(self.user.name, 'Han Solo')
 
-    def test_name_type(self):
+    def test_get_name_type(self):
         self.assertEqual(str, type(self.user.name))
