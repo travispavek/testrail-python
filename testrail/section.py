2478bd5ae419f8330ccda4e28d5f90adaa5609b0
Travis Pavek <travis@travispavek.com>
2016-02-07 15:22:47 -0600

    Add setters for post fields.

--- testrail/section.py
+++ testrail/section.py
@@ -1,4 +1,5 @@
 import api
+from helper import TestRailError
 from suite import Suite
 
 
@@ -23,13 +24,33 @@ class Section(object):
     def description(self):
         return self._content.get('description')
 
+    @description.setter
+    def description(self, value):
+        if type(value) != str:
+            raise TestRailError('input must be a string')
+        self._content['description'] = value
+
     @property
     def parent(self):
-        return self._content.get('parent_id')
+        return Section(
+            self.api.section_with_id(self._content.get('parent_id')))
+
+    @parent.setter
+    def parent(self, section):
+        if type(section) != Section:
+            raise TestRailError('input must be a Section')
+        self.api.section_with_id(section.id)  # verify section is valid
+        self._content['parent_id'] = section.id
 
     @property
     def name(self):
         return self._content.get('name')
+
+    @name.setter
+    def name(self, value):
+        if type(value) != str:
+            raise TestRailError('input must be a string')
+        self._content['name'] = value
 
     @property
     def suite(self):
@@ -37,3 +58,13 @@ class Section(object):
         if s:
             return Suite(s)
         return None
+
+    @suite.setter
+    def suite(self, suite_obj):
+        if type(suite_obj) != Suite:
+            raise TestRailError('input must be a Suite')
+        self.api.suite_with_id(suite_obj.id)  # verify suite is valid
+        self._content['suite_id'] = suite_obj.id
+
+    def raw_data(self):
+        return self._content
