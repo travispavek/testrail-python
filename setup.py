7fdac73fbbe5bc04cdf9b7c122d591bc721cfd30
Travis Pavek <travis@travispavek.com>
2016-02-11 08:17:40 -0600

    Version bump to 0.2.2

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name = 'testrail',
-    packages = ['testrail'],
-    version = '0.2.1',
-    description = 'Python library for interacting with TestRail via REST APIs.',
-    author = 'Travis Pavek',
-    author_email = 'travis.pavek@gmail.com',
-    url = 'https://github.com/travispavek/testrail-python',
-    download_url = 'https://github.com/travispavek/testrail-python/tarball/0.2.0',
-    keywords = ['testrail', 'api', 'client', 'library', 'rest'],
-    classifiers = [
+    name='testrail',
+    packages=['testrail'],
+    version='0.2.2',
+    description='Python library for interacting with TestRail via REST APIs.',
+    author='Travis Pavek',
+    author_email='travis.pavek@gmail.com',
+    url='https://github.com/travispavek/testrail-python',
+    download_url='https://github.com/travispavek/testrail-python/tarball/0.2.2',
+    keywords=['testrail', 'api', 'client', 'library', 'rest'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
