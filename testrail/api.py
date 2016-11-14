614effb5d1dd4d54285d853fd8a0d4a407b7adcc
Travis Pavek <travis.pavek@gmail.com>
2016-11-14 06:15:26 -0600

    added section methods in client.  fixed getting section with id when in multi suite mode

--- testrail/api.py
+++ testrail/api.py
@@ -368,6 +368,9 @@ class API(object):
             return list(filter(lambda x: x['id'] == section_id, self.sections()))[0]
         except IndexError:
             raise TestRailError("Section ID '%s' was not found" % section_id)
+        except TestRailError:
+            # project must not bein single suite mode
+            return self._get('get_section/%s' % section_id)
 
     def add_section(self, section):
         fields = ['description', 'suite_id', 'parent_id', 'name']
